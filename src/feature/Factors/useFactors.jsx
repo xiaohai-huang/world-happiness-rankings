ca89551172f72abc5443830bf1c13dee0659ac09
xiaohai-huang <jeromehuang18@gmail.com>
2021-05-03 16:52:24 +1000

    Debounce factor endpoint query (param : 'limit').

--- src/feature/Factors/useFactors.jsx
+++ src/feature/Factors/useFactors.jsx
@@ -1,16 +1,20 @@
 import { useEffect, useState } from "react";
 import API from "../../api/API";
+import useDebounce from "../../utility/useDebounce";
 
 function useFactors(year, { limit, country }) {
   const [factors, setFactors] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-
+  const debouncedLimit = useDebounce(limit, 300);
   useEffect(() => {
     (async () => {
       try {
         setLoading(true);
-        const data = await API.getFactors(year, { limit, country });
+        const data = await API.getFactors(year, {
+          limit: debouncedLimit,
+          country,
+        });
         setFactors(data);
         setError(null);
       } catch (err) {
@@ -19,7 +23,7 @@ function useFactors(year, { limit, country }) {
         setLoading(false);
       }
     })();
-  }, [year, limit, country]);
+  }, [year, debouncedLimit, country]);
 
   return { factors, loading, error };
 }
