334671b7d1d57b479e28718fde09b49958649855
xiaohai-huang <jeromehuang18@gmail.com>
2021-05-03 14:37:28 +1000

    Added token expiration support.

--- src/feature/User/AuthRoute.jsx
+++ src/feature/User/AuthRoute.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { useSnackbar } from "notistack";
 import { Redirect, Route } from "react-router";
+import jwt from "jsonwebtoken";
 
 function AuthRoute({ children, ...rest }) {
   const { enqueueSnackbar } = useSnackbar();
-
   return (
     <Route
       {...rest}
@@ -17,14 +17,37 @@ function AuthRoute({ children, ...rest }) {
             preventDuplicate: true,
           });
         }
+        // token exists then check if the token has expired
+        else if (!isTokenValid(token)) {
+          enqueueSnackbar("Token has expired, please login again!", {
+            variant: "warning",
+            preventDuplicate: true,
+          });
+          // clear the token
+          localStorage.removeItem("token");
+        }
+
         return token ? (
           React.cloneElement(children, routerProps)
         ) : (
-          <Redirect to="/" />
+          <Redirect to="/login" />
         );
       }}
     />
   );
 }
-
+// true if the toekn is valid
+function isTokenValid(token) {
+  // decode the token
+  const data = jwt.decode(token);
+  const currentDate = new Date();
+  const expireDate = new Date(data.exp * 1000); // missing 3 digits precision
+  console.log(currentDate);
+  console.log(expireDate);
+  if (expireDate - currentDate < 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
 export default AuthRoute;
